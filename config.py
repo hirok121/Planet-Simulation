260dc37eabab32638652172ad0b6c6a75e0f2b45
Hirok_Reza <hirokreza121@gmail.com>
2024-01-25 21:06:16 +0600

    simplest version

---
+++ config.py
@@ -0,0 +1,13 @@
+import pygame
+pygame.init()
+
+
+WIDTH, HEIGHT =  800, 800
+
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+BLUE = (100, 149, 237)
+RED = (188, 39, 50)
+DARK_GREY = (80, 78, 81)
+
+FONT = pygame.font.SysFont("comicsans", 16)
